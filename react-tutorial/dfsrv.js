2898366819008dadf5bace61d04f8e0febe6d1ff
Ubuntu <ubuntu@ip-172-31-41-237.us-east-2.compute.internal>
2019-07-21 03:07:02 +0000

    reverted from ssl

--- react-tutorial/dfsrv.js
+++ react-tutorial/dfsrv.js
@@ -1,64 +1,73 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer ( function(request,response){
+var server = http.createServer (
 
-    console.log("got request",request.method);
+	/*
+	{
+	  key: fs.readFileSync('/home/ubuntu/ssl/private-key.pem'),
+	  cert: fs.readFileSync('/home/ubuntu/ssl/252239439.crt')
+	},
+	*/
 
-    if(request.method == "GET")
-    {
-		console.log("get");
-    		response.writeHead(200,{"Content-Type":"text\plain"});
-            response.end("received GET request.")
-    }
-    else if(request.method == "POST")
-    {
-	console.log("post");
-	var query = require('url').parse(request.url,true).query;
-	console.log("query", query.id);
+    	function(request,response){
 
-	let body = '';
-	request.on('data', chunk => {
-        	body += chunk.toString(); // convert Buffer to string
-		console.log("chunk");
-    	});
-    	request.on('end', () => {
-        	console.log("body",body);
+	    console.log("got request",request.method);
 
-		obj = JSON.parse(body);
-		console.log(obj);
+	    if(request.method == "GET")
+	    {
+			console.log("get");
+			response.writeHead(200,{"Content-Type":"text\plain"});
+		    response.end("received GET request.")
+	    }
+	    else if(request.method == "POST")
+	    {
+		console.log("post");
+		var query = require('url').parse(request.url,true).query;
+		console.log("query", query.id);
 
-		dt = new Date().getTime()
-		fs.writeFileSync("./data/results_" + obj.id + "_" + dt, body,{encoding:'utf8',flag:'w'})
-		console.log("wrote file");
+		let body = '';
+		request.on('data', chunk => {
+			body += chunk.toString(); // convert Buffer to string
+			console.log("chunk");
+		});
+		request.on('end', () => {
+			console.log("body",body);
 
+			obj = JSON.parse(body);
+			console.log(obj);
+
+			dt = new Date().getTime()
+			fs.writeFileSync("./data/results_" + obj.id + "_" + dt, body,{encoding:'utf8',flag:'w'})
+			console.log("wrote file");
+
+			response.setHeader('Access-Control-Allow-Origin', '*');
+			response.setHeader('Access-Control-Request-Method', '*');
+			response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+			response.setHeader('Access-Control-Allow-Headers', request.headers.origin);
+			response.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
+			response.writeHead(200,{"Content-Type":"text\plain"});
+			response.end("received POST request.");
+		})
+	    }
+	    else if(request.method == "OPTIONS" )
+	    {
+		console.log("option", request.headers.origin);
 		response.setHeader('Access-Control-Allow-Origin', '*');
-        	response.setHeader('Access-Control-Request-Method', '*');
-        	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-        	response.setHeader('Access-Control-Allow-Headers', request.headers.origin);
+		response.setHeader('Access-Control-Request-Method', '*');
+		response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+		response.setHeader('Access-Control-Allow-Headers', request.headers.origin);
 		response.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
-    		response.writeHead(200,{"Content-Type":"text\plain"});
-            	response.end("received POST request.");
-	})
-    }
-    else if(request.method == "OPTIONS" )
-    {
-	console.log("option", request.headers.origin);
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader('Access-Control-Request-Method', '*');
-	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	response.setHeader('Access-Control-Allow-Headers', request.headers.origin);
-	response.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
-	if ( request.method === 'OPTIONS' ) {
-		response.writeHead(200);
-		response.end();
-		return;
-	}
-    }
-    else
-    {
-            response.end("Undefined request .");
-    }
+		if ( request.method === 'OPTIONS' ) {
+			response.writeHead(200);
+			response.end();
+			return;
+		}
+	    }
+	    else
+	    {
+		    response.end("Undefined request .");
+	    }
 });
 
 server.listen(3001);
